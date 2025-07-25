5714b8183ff8da4ac766a314194a3f0ef44475e4
ASHLEY RODRIGUEZ <ASHLEYMRODRIGUEZ@GMAIL.COM>
2025-07-25 16:29:19 -0400

    Added smooth transition to modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,26 @@
 const editProfileBtn = document.querySelector(".profile__edit");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionEl = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
+const newPostImageLinkInput = newPostModal.querySelector("#image-link-input");
+const newPostCaptionInput = newPostModal.querySelector("#caption-input");
+
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__description");
 
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  editProfileDescriptionEl.value = profileDescriptionEl.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +35,19 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(evt) {
+  evt.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  profileDescriptionEl.textContent = editProfileDescriptionEl.value;
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+
+function handleNewPostSave(evt) {
+  evt.preventDefault();
+  newPostModal.textContent = newPostImageLinkInput.value;
+  newPostModal.textContent = newPostCaptionInput.value;
+  newPostModal.classList.remove("modal_is-opened");
+}
