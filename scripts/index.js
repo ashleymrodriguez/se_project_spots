b5f3d9868b6572daa0f56a65203ee127d0ffecf3
ASHLEY RODRIGUEZ <ASHLEYMRODRIGUEZ@GMAIL.COM>
2025-07-30 22:30:07 -0400

    Added array loop

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: "Jellyfish",
+    link: "https://plus.unsplash.com/premium_photo-1685077721477-a4bc728512ef?q=80&w=772&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Hellblade's Senua",
+    link: "https://images.unsplash.com/photo-1583219083207-aefff43f327a?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Demon Goddess",
+    link: "https://images.unsplash.com/photo-1582272347045-fabd3d46679c?q=80&w=2152&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Hotel in Japan",
+    link: "https://images.unsplash.com/photo-1610338724170-5406cb1991ad?q=80&w=2672&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Blue Eyed White Dragon",
+    link: "https://images.unsplash.com/photo-1608133012729-7c76e221449c?q=80&w=1984&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Haunted Forest",
+    link: "https://images.unsplash.com/photo-1505635552518-3448ff116af3?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
+
 const editProfileBtn = document.querySelector(".profile__edit");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
@@ -53,3 +80,8 @@ function handleNewPostSave(evt) {
   console.log(newPostImageLinkInput.value);
   console.log(newPostCaptionInput.value);
 }
+
+initialCards.forEach(function (item) {
+  console.log(item.name);
+  console.log(item.link);
+});
